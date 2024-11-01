417a39314c1e483335d80175dc619ff8bdf88c0b
Leo S. <contato.prof.leonardo@gmail.com>
2024-11-01 16:59:17 -0400

    Criação da tela de login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,64 @@
-<x-guest-layout>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ asset('css/login.css') }}">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="row">
+          <!-- Left Blank Side -->
+          <div class="col-lg-6"></div>
+      
+          <!-- Right Side Form -->
+          <div class="col-lg-6 d-flex align-items-center justify-content-center right-side">
+            <div class="form-2-wrapper">
+              <div class="logo text-center mb-4">
+                <img src="{{ asset('/img/logo/logo-alone.png') }}" alt="">
+              </div>
+
+              <form action="#">
+                <div class="mb-3 form-box">
+                  <label for="email">CPF:</label>
+                  <input type="email" class="form-control" id="email" name="email" placeholder="000.000.000-00" required>
+                </div>
+                
+                <div class="mb-3">
+                    <label for="password">Senha:</label>
+                  <input type="password" class="form-control" id="password" name="password" placeholder="Mínimo de 8 caracteres" required>
+                </div>
+
+                {{-- <div class="mb-3">
+                  <div>
+                    <input type="checkbox" class="form-check-input" id="rememberMe">
+                    <label class="form-check-label" for="rememberMe">Remember me</label>
+                  </div>
+                </div> --}}
+
+                <button type="submit" class="login-btn w-50 mt-4 mb-3">Entrar</button>
+              </form>
+      
+              <!-- Register Link -->
+              <div class="text-start register-link mt-4">
+                <a href="#" class="">Esqueceu a senha?</a>
+                <p class="mt-1">Primeira vez usando o Agendaí? <a href="#" class="">Cadastre-se</a></p>
+              </div>
+              
+            </div>
+          </div>
+        </div>
+      </div>
+</body>
+</html>
+
+
+
+{{-- <x-guest-layout>
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
 
@@ -44,4 +104,4 @@
             </x-primary-button>
         </div>
     </form>
-</x-guest-layout>
+</x-guest-layout> --}}
